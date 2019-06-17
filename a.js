be75f804fdbc45d9660351bf3f56cc890f6409bf
EmillyTavares <50457025+EmillyTavares@users.noreply.github.com>
2019-06-17 12:42:40 -0300

    Add files via upload

---
+++ a.js
@@ -0,0 +1,9 @@
+function mostraSub(sub){
+  subfilho = document.getElementById(sub);
+  subfilho.style.display = 'block';
+}
+
+function escondeSub(sub){
+  subfilho = document.getElementById(sub);
+  subfilho.style.display = 'none'; 
+}
